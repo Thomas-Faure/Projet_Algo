08a22fd305fc34483ce5bc0d3a75a0937460149f
LucasPln <lucas.paulin@hotmail.fr>
2018-12-27 15:38:47 +0100

    Ajout des get et set pour avoir les positions en X et Y

--- Projet_GroupeB/Librairie/Sources/Librairie/Position.swift
+++ Projet_GroupeB/Librairie/Sources/Librairie/Position.swift
@@ -1,17 +1,29 @@
 public class PositionClass : PositionProtocol{
   public typealias Position = PositionClass
+
   public var position = (0,0)
+  
   public required init(x : Int,y :Int){
     position.0 = x
     position.1 = y
   }
+  
+  @discardableResult
   public func Change_Position(posfin:Position?)->Self{
-  if let b = posfin {
-    position.0 = b.position.0
-    position.1 = b.position.1
-    return self
-  } else {
-    return self
+    if let b = posfin {
+      position.0 = b.position.0
+      position.1 = b.position.1
+      return self
+    } else {
+      return self
+    }
   }
+
+  public func getX()->Int{
+    return position.0
+  }
+
+  public func getY()->Int{
+    return position.1
   }
 }
